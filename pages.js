a8024c5f61f4d69ac472b3f0c436fb81e7a4981c
Name Is Matt <github.com@selif.net>
2015-04-06 21:20:35 -0600

    Added to template
    Added textarea that will be the page's editor. It contains the page
    content in raw form to be edited by the user.  Normally, the source and
    editor section of the template will be invisible until the user has
    entered the edit mode.

--- pages.js
+++ pages.js
@@ -1,15 +1,15 @@
 var book = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\
 <book>\
 <page title='::template'>\
-<content><![CDATA[<div id=\"page_title\">{{=page_title}}</div><div id=\"page_content\">{{=page_content}}</div>]]></content>\
+<content><![CDATA[<div id=\"header_wrap\"><div id=\"page_title\">{{=page_title}}</div></div><div id=\"content_wrap\"><div id=\"page_content\">{{=page_content}}</div></div><div id=\"source_wrap\"><textarea id=\"page_source\" readonly>{{=page_content_raw}}</textarea></div><div id=\"editor_wrap\"><textarea id=\"page_editor\">{{=page_content_raw}}</textarea><br /><button id=\"editor_save\">Save</button></div>]]></content>\
 </page>\
 \
 <page title='::css'>\
-<content><![CDATA[body { }]]></content>\
+<content><![CDATA[/* core stylesheet */\nbody { }]]></content>\
 </page>\
 \
 <page title='main page'>\
-<content><![CDATA[= Heading 1\n\n[[Link]]\n[[Link|Link with label]]\nPlain text\n*Bold text*\n!Strong text!\n/Italic text/\n//Emphasized text//\n_Underline text_\n-Strikethough text-\n--Deleted text--]]></content>\
+<content><![CDATA[= Heading 1\n\n[[Link]]\n[[Link|Link with label]]\nPlain text\n*Bold text*\n!Strong text!\n/Italic text/\n//Emphasized text//\n_Underline text_\n-Strikethrough text-\n--Deleted text--]]></content>\
 </page>\
 \
 <page title='namespace::page'>\
